9d8df37fb1d8bc12c7a6faecf143b77b1a022aac
David Cox <davidcox@me.com>
2011-09-08 16:28:47 -0400

    Updated patch resulting from correspondence with library author.

--- Library/Formula/anttweakbar.rb
+++ Library/Formula/anttweakbar.rb
@@ -20,9 +20,44 @@ class Anttweakbar < Formula
 
     # patch two small issues -- one related to 10.7 compatibility, one related
     # to missing symbols due to an (apparently) erroneous 'extern "C"' wrapper
-  'https://raw.github.com/gist/1182684/8afd29c2bef6c410f1886d297012af752c0d0ac3/atb_10_7_patch_minimal.diff'
+    DATA
+
   end
 end
+
+__END__
+diff --git a/src/LoadOGL.h b/src/LoadOGL.h
+index 18dd25d..db05586 100755
+--- a/src/LoadOGL.h
++++ b/src/LoadOGL.h
+@@ -346,7 +346,7 @@ ANT_GL_DECL(void, glTexGenf, (GLenum coord, GLenum pname, GLfloat param))
+ ANT_GL_DECL(void, glTexGenfv, (GLenum coord, GLenum pname, const GLfloat *params))
+ ANT_GL_DECL(void, glTexGeni, (GLenum coord, GLenum pname, GLint param))
+ ANT_GL_DECL(void, glTexGeniv, (GLenum coord, GLenum pname, const GLint *params))
+-#if defined(ANT_OSX)
++#if defined(ANT_OSX) && (MAC_OS_X_VERSION_MAX_ALLOWED < 1070) // Mac OS X 10.7
+ // Mac OSX redefined these OpenGL calls: glTexImage1D, glTexImage2D
+ ANT_GL_DECL(void, glTexImage1D, (GLenum target, GLint level, GLenum internalformat, GLsizei width, GLint border, GLenum format, GLenum type, const GLvoid *pixels))
+ ANT_GL_DECL(void, glTexImage2D, (GLenum target, GLint level, GLenum internalformat, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid *pixels))
+diff --git a/src/TwEventSDL.c b/src/TwEventSDL.c
+index 5371dde..5806f34 100755
+--- a/src/TwEventSDL.c
++++ b/src/TwEventSDL.c
+@@ -18,7 +18,14 @@
+ 
+ int TW_CALL TwEventSDL12(const void *sdlEvent); // implemented in TwEventSDL12.c
+ int TW_CALL TwEventSDL13(const void *sdlEvent); // implmeneted in TwEventSDL13.c
++#ifdef __cplusplus
++extern "C" {
++#endif
+ int TW_CALL TwSetLastError(const char *staticErrorMessage);
++#ifdef __cplusplus
++}
++#endif
++
+ 
+ //  TwEventSDL returns zero if msg has not been handled or the SDL version 
+ //  is not supported, and a non-zero value if it has been handled by the 
 
 
 
